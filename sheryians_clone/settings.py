11347fafd0919be414addab13b8eee2bb8752234
20481a05e7 <20481a05e7@gecgudlavallerumic.in>
2024-12-23 08:21:51 +0530

    commit3

--- sheryians_clone/settings.py
+++ sheryians_clone/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 import os
+TEMPLATE_DIR=os.path.join(BASE_DIR,'template')
 STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles')
 
 STATIC_URL = "static/"
